8b5e0062d178d5ffb803951f3ceb1df5dc929e19
Nafis17 <sabitgt17@gmail.com>
2021-06-22 11:40:30 +0800

    1

--- src/object/Class_1.java
+++ src/object/Class_1.java
@@ -1,25 +1,18 @@
 package object;
 
-import javax.swing.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-
 public class Class_1 {
 
     public String nama;
     public String npm;
     public String kelas;
     public String namamakanan;
-    public String mk1, mk2, mk3, mk4;
-    public String mn1, mn2, mn3, mn4;
+    public String menu1, menu2, menu3, menu4;
+    public String minuman1, minuman2, minuman3, minuman4;
     public String namaminuman;
+    public String hmenu1, hmenu2, hmenu3, hmenu4;
+    public String hminuman1, hminuman2, hminuman3, hminuman4;
     public String jumlahmakanan;
     public String jumlahminuman;
-    public String jenismakanan;
-    public String jenisminuman;
-    public String hm1, hm2, hm3, hm4;
-    public String hmnm1, hmnm2, hmnm3, hmnm4;
-
 
     public String getNama() {
         return nama;
@@ -53,68 +46,68 @@ public class Class_1 {
         this.namamakanan = namamakanan;
     }
 
-    public String getMk1() {
-        return mk1;
+    public String getMenu1() {
+        return menu1;
     }
 
-    public void setMk1(String mk1) {
-        this.mk1 = mk1;
+    public void setMenu1(String menu1) {
+        this.menu1 = menu1;
     }
 
-    public String getMk2() {
-        return mk2;
+    public String getMenu2() {
+        return menu2;
     }
 
-    public void setMk2(String mk2) {
-        this.mk2 = mk2;
+    public void setMenu2(String menu2) {
+        this.menu2 = menu2;
     }
 
-    public String getMk3() {
-        return mk3;
+    public String getMenu3() {
+        return menu3;
     }
 
-    public void setMk3(String mk3) {
-        this.mk3 = mk3;
+    public void setMenu3(String menu3) {
+        this.menu3 = menu3;
     }
 
-    public String getMk4() {
-        return mk4;
+    public String getMenu4() {
+        return menu4;
     }
 
-    public void setMk4(String mk4) {
-        this.mk4 = mk4;
+    public void setMenu4(String menu4) {
+        this.menu4 = menu4;
     }
 
-    public String getMn1() {
-        return mn1;
+    public String getMinuman1() {
+        return minuman1;
     }
 
-    public void setMn1(String mn1) {
-        this.mn1 = mn1;
+    public void setMinuman1(String minuman1) {
+        this.minuman1 = minuman1;
     }
 
-    public String getMn2() {
-        return mn2;
+    public String getMinuman2() {
+        return minuman2;
     }
 
-    public void setMn2(String mn2) {
-        this.mn2 = mn2;
+    public void setMinuman2(String minuman2) {
+        this.minuman2 = minuman2;
     }
 
-    public String getMn3() {
-        return mn3;
+    public String getMinuman3() {
+        return minuman3;
     }
 
-    public void setMn3(String mn3) {
-        this.mn3 = mn3;
+    public void setMinuman3(String minuman3) {
+        this.minuman3 = minuman3;
     }
 
-    public String getMn4() {
-        return mn4;
+    public String getMinuman4() {
+        return minuman4;
     }
 
-    public void setMn4(String mn4) {
-        this.mn4 = mn4;
+    public void setMinuman4(String minuman4) {
+        this.minuman4 = minuman4;
     }
 
     public String getNamaminuman() {
@@ -123,6 +116,70 @@ public class Class_1 {
 
     public void setNamaminuman(String namaminuman) {
         this.namaminuman = namaminuman;
+    }
+
+    public String getHmenu1() {
+        return hmenu1;
+    }
+
+    public void setHmenu1(String hmenu1) {
+        this.hmenu1 = hmenu1;
+    }
+
+    public String getHmenu2() {
+        return hmenu2;
+    }
+
+    public void setHmenu2(String hmenu2) {
+        this.hmenu2 = hmenu2;
+    }
+
+    public String getHmenu3() {
+        return hmenu3;
+    }
+
+    public void setHmenu3(String hmenu3) {
+        this.hmenu3 = hmenu3;
+    }
+
+    public String getHmenu4() {
+        return hmenu4;
+    }
+
+    public void setHmenu4(String hmenu4) {
+        this.hmenu4 = hmenu4;
+    }
+
+    public String getHminuman1() {
+        return hminuman1;
+    }
+
+    public void setHminuman1(String hminuman1) {
+        this.hminuman1 = hminuman1;
+    }
+
+    public String getHminuman2() {
+        return hminuman2;
+    }
+
+    public void setHminuman2(String hminuman2) {
+        this.hminuman2 = hminuman2;
+    }
+
+    public String getHminuman3() {
+        return hminuman3;
+    }
+
+    public void setHminuman3(String hminuman3) {
+        this.hminuman3 = hminuman3;
+    }
+
+    public String getHminuman4() {
+        return hminuman4;
+    }
+
+    public void setHminuman4(String hminuman4) {
+        this.hminuman4 = hminuman4;
     }
 
     public String getJumlahmakanan() {
@@ -141,95 +198,17 @@ public class Class_1 {
         this.jumlahminuman = jumlahminuman;
     }
 
-    public String getJenismakanan() {
-        return jenismakanan;
-    }
 
-    public void setJenismakanan(String jenismakanan) {
-        this.jenismakanan = jenismakanan;
-    }
-
-    public String getJenisminuman() {
-        return jenisminuman;
-    }
-
-    public void setJenisminuman(String jenisminuman) {
-        this.jenisminuman = jenisminuman;
-    }
-
-    public String getHm1() {
-        return hm1;
-    }
-
-    public void setHm1(String hm1) {
-        this.hm1 = hm1;
-    }
-
-    public String getHm2() {
-        return hm2;
-    }
-
-    public void setHm2(String hm2) {
-        this.hm2 = hm2;
-    }
-
-    public String getHm3() {
-        return hm3;
-    }
-
-    public void setHm3(String hm3) {
-        this.hm3 = hm3;
-    }
-
-    public String getHm4() {
-        return hm4;
-    }
-
-    public void setHm4(String hm4) {
-        this.hm4 = hm4;
-    }
-
-    public String getHmnm1() {
-        return hmnm1;
-    }
-
-    public void setHmnm1(String hmnm1) {
-        this.hmnm1 = hmnm1;
-    }
-
-    public String getHmnm2() {
-        return hmnm2;
-    }
-
-    public void setHmnm2(String hmnm2) {
-        this.hmnm2 = hmnm2;
-    }
-
-    public String getHmnm3() {
-        return hmnm3;
-    }
-
-    public void setHmnm3(String hmnm3) {
-        this.hmnm3 = hmnm3;
-    }
-
-    public String getHmnm4() {
-        return hmnm4;
-    }
-
-    public void setHmnm4(String hmnm4) {
-        this.hmnm4 = hmnm4;
-    }
 
 
     public void menuFood() {
         System.out.println("================================");
         System.out.println(" Food ");
         System.out.println("================================");
-        System.out.println("1." + mk1);
-        System.out.println("2." + mk2);
-        System.out.println("3." + mk3);
-        System.out.println("4." + mk4);
+        System.out.println("1." + menu1);
+        System.out.println("2." + menu2);
+        System.out.println("3." + menu3);
+        System.out.println("4." + menu4);
 
 
     }
@@ -238,31 +217,31 @@ public class Class_1 {
         System.out.println("================================");
         System.out.println(" Drink ");
         System.out.println("================================");
-        System.out.println("1:" + mn1);
-        System.out.println("2." + mn2);
-        System.out.println("3." + mn3);
-        System.out.println("4." + mn4);
+        System.out.println("1:" + minuman1);
+        System.out.println("2." + minuman2);
+        System.out.println("3." + minuman3);
+        System.out.println("4." + minuman4);
 
     }
 
-    public void hrgFood() {
+    public void hrgFood(String s) {
         System.out.println("================================");
         System.out.println(" Harga Food ");
         System.out.println("================================");
-        System.out.println("1. " + hm1);
-        System.out.println("2." + hm2);
-        System.out.println("3. " + hm3);
-        System.out.println("4. " + hm4);
+        System.out.println("1. " + hmenu1);
+        System.out.println("2." + hmenu2);
+        System.out.println("3. " + hmenu3);
+        System.out.println("4. " + hmenu4);
     }
 
-    public void hrgDrink() {
+    public void hrgDrink(String s) {
         System.out.println("================================");
         System.out.println(" Harga Drink  ");
         System.out.println("================================");
-        System.out.println("1." + hmnm1);
-        System.out.println("2. " + hmnm2);
-        System.out.println("3. " + hmnm3);
-        System.out.println("4. " + hmnm4);
+        System.out.println("1." + hminuman1);
+        System.out.println("2. " + hminuman2);
+        System.out.println("3. " + hminuman3);
+        System.out.println("4. " + hminuman4);
     }
 
     public void tampilkanMenu() {
@@ -272,9 +251,8 @@ public class Class_1 {
         System.out.println("Nama Makanan : " + namamakanan);
         System.out.println("Nama Minuman : " + namaminuman);
         System.out.println("Jumlah Makanan : " + jumlahmakanan);
-        System.out.println("Jenis Minuman  : " + jenisminuman);
-        System.out.println("Harga Makanan  : " + hm1);
-        System.out.println("Harga Minuman  : " + hmnm1);
+        System.out.println("Harga Makanan  : " + hmenu1);
+        System.out.println("Harga Minuman  : " + hminuman1);
     }
 
 
